2f013241240b8dc62307899ec96098d6f290050c
zares <sergiozares@gmail.com>
2025-11-17 14:08:53 +0200

    Code refactoring.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Mako helpers
+# Mako helpers extended
 
 Helper functions for the Mako Framework
 
@@ -6,25 +6,21 @@ Helper functions for the Mako Framework
 |---------------------------------------|--------------------------------------------------------------|
 | app()                                 | Returns the `application` instance                           |
 | container()                           | Returns the `mako\syringe\Container` instance                |
-| config($key=null, $default=null)      | Returns the `mako\config\Config` instance                    |
+| config($key=null, $default=null)      | Returns the `mako\config\Config` instance or set|get value   |
 | request()                             | Returns the `mako\http\Request` instance                     |
 | turbo_frame()                         | Returnss the Turbo-Frame header value                        |
 | cookie($name, $default=null)          | Returns the cookie value or null if the cookie doesn't exist |
 | signed_cookie($name, $default=null)   | Returns the cookie value or null if the cookie doesn't exist |
 | url()                                 | Returns the `mako\http\routing\URLBuilder` instance          |
-| route($route, $params=[])             | Returns the URL of a named route                             |
+| route($route, $params=[], ...)        | Returns the URL of a named route                             |
 | session()                             | Returns the `mako\session\Session` instance                  |
-| flashdata()                           | Returns a flash data as array from the session               |
-| flash($key, $default=null)            | Returns a flash value from the session                       |
+| flashdata()                           | Returns a flash data as array from the session               |
+| flash($key, $default=null)            | Returns a flash value from the session                       |
 | token()                               | Returns the session token                                    |
 | one_time_token()                      | Returns a random security token                              |
-| gatekeeper($adapterName=null)         | Returns a gatekeeper adapter instance                        |
+| gatekeeper($adapterName=null)         | Returns a gatekeeper adapter instance                        |
 | user($adapterName=null)               | Returns the active user or null if there isn't one           |
-| mix($path, $manifestDirectory='')     | Returns the path to a versioned Mix file                     |
-
-## Requirements
-
-Mako 9.1 or greater.
+| vite($path, $manifestDirectory='')    | Returns the path to a versioned Vite file                    |
 
 ## Installation
 
--- src/helpers.php
+++ src/helpers.php
@@ -1,12 +1,14 @@
 <?php
 
 use mako\application\Application;
+use mako\application\CurrentApplication;
 use mako\gatekeeper\adapters\AdapterInterface;
 use mako\http\routing\URLBuilder;
 use mako\gatekeeper\Gatekeeper;
 use mako\syringe\Container;
 use mako\session\Session;
 use mako\http\Request;
+
 
 // Application
 if (function_exists('app') === false) {
@@ -19,7 +21,7 @@ if (function_exists('app') === false) {
 		static $app;
 
 		if ($app === null) {
-			$app = Application::instance();
+			$app = CurrentApplication::get();
 		}
 
 		return $app;
@@ -37,7 +39,7 @@ if (function_exists('container') === false) {
 		static $container;
 
 		if ($container === null) {
-			$container = Application::instance()->getContainer();
+			$container = app()->getContainer();
 		}
 
 		return $container;
@@ -48,8 +50,8 @@ if (function_exists('container') === false) {
 if (function_exists('config') === false) {
 	/**
 	 * Returns the config instance.
-     * @param  array|string|null  $key
-     * @param  mixed  $default
+	 * @param  array|string|null  $key
+	 * @param  mixed  $default
 	 * @return mixed|\mako\config\Config
 	 */
 	function config($key = null, $default = null)
@@ -57,18 +59,18 @@ if (function_exists('config') === false) {
 		static $config;
 
 		if ($config === null) {
-			$config = Application::instance()->getConfig();
+			$config = app()->getConfig();
 		}
 
-        if (is_null($key)) {
-            return $config;
-        }
+		if (is_null($key)) {
+			return $config;
+		}
 
-        if (is_array($key)) {
-            return $config->set($key);
-        }
+		if (is_array($key)) {
+			return $config->set($key);
+		}
 
-        return $config->get($key, $default);
+		return $config->get($key, $default);
 	}
 }
 
@@ -83,7 +85,7 @@ if (function_exists('request') === false) {
 		static $request;
 
 		if ($request === null) {
-			$request = Application::instance()->getContainer()->get(Request::class);
+			$request = container()->get(Request::class);
 		}
 
 		return $request;
@@ -129,105 +131,105 @@ if (function_exists('request') === false) {
 
 // UrlBuilder
 if (function_exists('url') === false) {
-    /**
-     * Returns the url builder instance.
-     * @return \mako\http\routing\URLBuilder
-     */
-    function url(): URLBuilder
-    {
-        static $url;
+	/**
+	 * Returns the url builder instance.
+	 * @return \mako\http\routing\URLBuilder
+	 */
+	function url(): URLBuilder
+	{
+		static $url;
 
-        if ($url === null) {
-            $url = Application::instance()->getContainer()->get(URLBuilder::class);
-        }
+		if ($url === null) {
+			$url = container()->get(URLBuilder::class);
+		}
 
-        return $url;
-    }
+		return $url;
+	}
 
-    if (function_exists('route') === false) {
-        /**
-         * Returns the URL of a named route.
-         * @param  string      $route       Route name
-         * @param  array       $params      Route parameters
-         * @param  array       $query       Associative array used to build URL-encoded query string
-         * @param  string      $separator   Argument separator
-         * @param  bool|string $language    Request language
-         * @return string
-         */
-        function route(
-            string $route, array $params = [], array $query = [],
-            string $separator = '&', bool|string $language = true
-        ): string
-        {
-            return url()->toRoute(
-                $route, $params, $query, $separator, $language
-            );
-        }
-    }
+	if (function_exists('route') === false) {
+		/**
+		 * Returns the URL of a named route.
+		 * @param  string      $route       Route name
+		 * @param  array       $params      Route parameters
+		 * @param  array       $query       Associative array used to build URL-encoded query string
+		 * @param  string      $separator   Argument separator
+		 * @param  bool|string $language    Request language
+		 * @return string
+		 */
+		function route(
+			string $route, array $params = [], array $query = [],
+			string $separator = '&', bool|string $language = true
+		): string
+		{
+			return url()->toRoute(
+				$route, $params, $query, $separator, $language
+			);
+		}
+	}
 }
 
 // Session
 if (function_exists('session') === false) {
-    /**
-     * Returns the session instance.
-     * @return \mako\session\Session
-     */
-    function session(): Session
-    {
-        static $session;
+	/**
+	 * Returns the session instance.
+	 * @return \mako\session\Session
+	 */
+	function session(): Session
+	{
+		static $session;
 
-        if ($session === null) {
-            $session = Application::instance()->getContainer()->get(Session::class);
-        }
+		if ($session === null) {
+			$session = container()->get(Session::class);
+		}
 
-        return $session;
-    }
+		return $session;
+	}
 
-    if (function_exists('flashdata') === false) {
-        /**
-         * Returns a flash data from the session.
-         * @return array
-         */
-        function flashdata()
-        {
-            return session()->get('mako.flashdata', []);
-        }
-    }
+	if (function_exists('flashdata') === false) {
+		/**
+		 * Returns a flash data from the session.
+		 * @return array
+		 */
+		function flashdata()
+		{
+			return session()->get('mako.flashdata', []);
+		}
+	}
 
-    if (function_exists('flash') === false) {
-        /**
-         * Returns a flash value from the session.
-         * @param  string $key     Session key
-         * @param  mixed  $default Default value
-         * @return mixed
-         */
-        function flash(string $key, $default = null)
-        {
-            return session()->getFlash($key, $default);
-        }
-    }
+	if (function_exists('flash') === false) {
+		/**
+		 * Returns a flash value from the session.
+		 * @param  string $key     Session key
+		 * @param  mixed  $default Default value
+		 * @return mixed
+		 */
+		function flash(string $key, $default = null)
+		{
+			return session()->getFlash($key, $default);
+		}
+	}
 
-    if (function_exists('token') === false) {
-        /**
-         * Returns the session token.
-         * @return string
-         */
-        function token(): string
-        {
-            return session()->getToken();
-        }
-    }
+	if (function_exists('token') === false) {
+		/**
+		 * Returns the session token.
+		 * @return string
+		 */
+		function token(): string
+		{
+			return session()->getToken();
+		}
+	}
 
-    if (function_exists('one_time_token') === false) {
-        /**
-         * Returns a random security token.
-         * @return string
-         */
-        function one_time_token(): string
-        {
-            return session()->generateOneTimeToken();
-        }
-    }
+	if (function_exists('one_time_token') === false) {
+		/**
+		 * Returns a random security token.
+		 * @return string
+		 */
+		function one_time_token(): string
+		{
+			return session()->generateOneTimeToken();
+		}
+	}
 }
 
 // Gatekeeper
@@ -237,12 +239,12 @@ if (function_exists('gatekeeper') === false) {
 	 * @param  string|null       $adapterName Adapter name
 	 * @return \mako\gatekeeper\adapters\AdapterInterface
 	 */
-	function auth(?string $adapterName = null): AdapterInterface
+	function gatekeeper(?string $adapterName = null): AdapterInterface
 	{
 		static $gatekeeper;
 
-		if($gatekeeper === null) {
-			$gatekeeper = Application::instance()->getContainer()->get(Gatekeeper::class);
+		if ($gatekeeper === null) {
+			$gatekeeper = container()->get(Gatekeeper::class);
 		}
 
 		return $gatekeeper->adapter($adapterName);
@@ -256,46 +258,40 @@ if (function_exists('gatekeeper') === false) {
 		 */
 		function user(?string $adapterName = null)
 		{
-			return auth($adapterName)->getUser();
+			return gatekeeper($adapterName)->getUser();
 		}
 	}
 }
 
-// Mix
-if (function_exists('mix') === false) {
+// Vite
+if (function_exists('vite') === false) {
 	/**
-	 * Get the path to a versioned Mix file.
+	 * Get the path to a versioned Vite file.
 	 * @param  string  $path
 	 * @param  string  $manifestDirectory
 	 * @return string
 	 */
-	function mix($path, $manifestDirectory = '')
+	function vite($path, $manifestDirectory = 'build')
 	{
-	    static $manifests = [];
+		static $manifests = [];
 
-	    $publicPath = str_replace(DIRECTORY_SEPARATOR .'app', DIRECTORY_SEPARATOR .'public', MAKO_APPLICATION_PATH);
-	    $manifestPath = $publicPath . $manifestDirectory .'/mix-manifest.json';
+		$publicPath = str_replace(DIRECTORY_SEPARATOR .'app', DIRECTORY_SEPARATOR .'public', MAKO_APPLICATION_PATH);
+		$manifestPath = $publicPath . DIRECTORY_SEPARATOR . $manifestDirectory . DIRECTORY_SEPARATOR .'manifest.json';
 
-	    if (! isset($manifests[$manifestPath])) {
-	        if (! is_file($manifestPath)) {
-	            throw new \Exception('The Mix manifest does not exist.');
-	        }
+		if (! isset($manifests[$manifestPath])) {
+			if (! is_file($manifestPath)) {
+				throw new \Exception('The manifest file does not exist.');
+			}
 
-	        $manifests[$manifestPath] = json_decode(file_get_contents($manifestPath), true);
-	    }
+			$manifests[$manifestPath] = json_decode(file_get_contents($manifestPath), true);
+		}
 
-	    $manifest = $manifests[$manifestPath];
+		$manifest = $manifests[$manifestPath];
 
-	    if (! isset($manifest[$path])) {
-	        $config = \mako\application\Application::instance()->getConfig();
+		if (! isset($manifest[$path])) {
+			throw new \Exception("Unable to locate Vite file: {$path}.");
+		}
 
-	        if (! $config->get('application.error_handler.display_errors')) {
-	            return $manifestDirectory . $path;
-	        } else {
-	            throw new \Exception("Unable to locate Mix file: {$path}.");
-	        }
-	    }
-
-	    return $manifestDirectory . $manifest[$path];
+		return '/'. $manifestDirectory .'/'. $manifest[$path]['file'];
 	}
 }
